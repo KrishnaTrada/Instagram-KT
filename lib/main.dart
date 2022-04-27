68dc32a726e0e3c72c8bfa2839336a09a82434e6
KrishnaTrada <kvtrada403@gmail.com>
2022-04-27 13:24:42 +0530

    firebase setup completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_kt/responsive/mobile_screen_layout.dart';
 import 'package:instagram_kt/responsive/responsive_layout_screen.dart';
 import 'package:instagram_kt/responsive/web_screen_layout.dart';
 import 'package:instagram_kt/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDX32kQMoZgSgFfepQZ2fHHAdVZD7TDBNM",
+            appId: "1:59258408310:web:9ee006bc812f6a88633e57",
+            messagingSenderId: "59258408310",
+            projectId: "instagram-kt",
+            storageBucket: "instagram-kt.appspot.com"));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -20,8 +34,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
       home: const ResponsiveLayout(
-              webScreenLayout: WebScreenLayout(),
-              mobileScreenLayout: MobileScreenLayout()),
-      );
+          webScreenLayout: WebScreenLayout(),
+          mobileScreenLayout: MobileScreenLayout()),
+    );
   }
 }
